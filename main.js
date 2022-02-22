14d9a9163565b50d6d508f2f6e17c228baa772d0
Dale Kittendorf <drkittendorf@gmail.com>
2022-02-21 18:06:58 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Set my fictitious age
+let myAge = 19;
+// declare my early years
+let earlyYears = 2;
+// update my early years to 10.5 times
+earlyYears = earlyYears * 10.5;
+// subtracting 2 from my fictitious age to equal later years??
+let laterYears =  myAge - 2;
+// multiplying later years by 4 to equal dog years
+let dogYears = laterYears * 4;
+//adding items together per instructions, to create a new variable
+let MyAgeInDogYears = earlyYears + laterYears;
+
+console.log (earlyYears, laterYears)
+// creating a name variable then converting to lowercase
+let myName = "Dale";
+myName = myName.toLowerCase();
+console.log(myName)
+// sending to console a template literal of our variables in sentence form.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${MyAgeInDogYears} years old in dog years.`)
+
+
+
